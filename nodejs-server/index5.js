cf2fe3f6538ea31a8e69da98cc1875f1288c7f94
donutz03 <109514731+donutz03@users.noreply.github.com>
2023-10-09 09:20:00 +0300

    Update index5.js

--- nodejs-server/index5.js
+++ nodejs-server/index5.js
@@ -1,1 +1,9 @@
-
+const rubiksCubeFacts = {
+  possiblePermutations: '1,2,3,55,6',
+  invented: '1974',
+  largestCube: '17x17x17'
+};
+const {possiblePermutations,invented,largestCube} = rubiksCubeFacts;
+console.log(possiblePermutations); // '1,2,3,55,6'
+console.log(invented); //'1974'
+console.log(largestCube); // '17x17x17'
