65eb64dcbcdc8a453239aa23686ca4b4f8789586
donutz03 <109514731+donutz03@users.noreply.github.com>
2023-10-13 17:04:27 +0300

    Create product.model.ts

---
+++ validare date/src/product.model.ts
@@ -0,0 +1,38 @@
+import {Contains,IsInt,IsOptional,IsString,Min,MinLength} from 'class-validator';
+
+export class ProductModel{
+
+
+
+  @IsOptional()
+  @IsInt()
+  @IsPositive()
+  id: number;
+  
+  @MinLength(5)
+  @IsString()
+  name: string;
+
+  @IsNumber()
+  @IsPositive()
+  @Min(1000)
+  quantity: number;
+
+  @IsNumber()
+  @IsPositive()
+  @Min(1)
+  price: number;
+  
+  @IsString()
+  @Contains('SC')
+  @MinLength(10)
+  price:number;
+  
+  @IsString()
+  @Contains('SC')
+  @MinLength(10)
+  vendor:string;
+  constructor(obj:any) {
+    Object.assign(this,obj);
+  }
+}
